4c2df63368f66b05154b94264a88ff80ac670810
bx2k <BillXu2000@126.com>
2022-02-22 19:10:57 +0800

    json2meta

--- meshtaichi_patcher/__init__.py
+++ meshtaichi_patcher/__init__.py
@@ -1,1 +1,9 @@
 from meshtaichi_patcher_core import *
+import json
+
+def mesh2meta(filename, relations):
+    import taichi as ti
+    patcher = Patcher()
+    patcher.run(filename, relations)
+    data = json.loads(patcher.export_json())
+    return ti.Mesh.json2meta(data)
