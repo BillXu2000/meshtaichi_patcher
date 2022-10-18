cc000a69b718a0434019d5e8815b2680392e225b
bx2k <BillXu2000@126.com>
2022-10-18 12:17:22 +0800

    fix command

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 import subprocess
 
-subprocess.run('rm dist/* -y')
+subprocess.run('rm -r dist', shell=True)
 
 for i in range(7, 11):
     cmd = f'zsh -c "source ~/.zshrc; conda activate py3{i}; python3 setup.py sdist bdist_wheel -p manylinux_2_27_x86_64"'
--- build_m1.py
+++ build_m1.py
@@ -1,7 +1,7 @@
 import subprocess
 
-subprocess.run('rm dist/* -y')
-subprocess.run('git pull')
+subprocess.run('rm -r dist', shell=True)
+subprocess.run('git pull', shell=True)
 
 for i in range(8, 11):
     cmd = f'zsh -c "source ~/.zshrc; conda activate py3{i}; python3.{i} setup.py sdist bdist_wheel"'
