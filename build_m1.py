2a1e1d35bc1528a0e8528d9acc15892ba5a9f415
bx2k <BillXu2000@126.com>
2022-10-18 12:18:48 +0800

    fix pull priority

--- build_m1.py
+++ build_m1.py
@@ -1,7 +1,7 @@
 import subprocess
 
-subprocess.run('rm -r dist', shell=True)
 subprocess.run('git pull', shell=True)
+subprocess.run('rm -r dist', shell=True)
 
 for i in range(8, 11):
     cmd = f'zsh -c "source ~/.zshrc; conda activate py3{i}; python3.{i} setup.py sdist bdist_wheel"'
