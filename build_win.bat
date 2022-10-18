e3aae22cb3e2f87e61da53f72c81055ce8157d6f
bx2k <BillXu2000@126.com>
2022-10-18 17:08:44 +0800

    add a hack win bat

---
+++ build_win.bat
@@ -0,0 +1,5 @@
+conda activate py37 ; python setup.py bdist_wheel ;
+conda activate py38 ; python setup.py bdist_wheel ;
+conda activate py39 ; python setup.py bdist_wheel ;
+conda activate py310 ; python setup.py bdist_wheel ;
+
